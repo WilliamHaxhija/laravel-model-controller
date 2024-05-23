82078908d6c5cd2ffb461a552c63128fe603fb64
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-23 18:08:09 +0200

    end project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,26 @@
 
 @section('content')
     <h1>HomePage</h1>
+    <div class="container">
+        <div class="row g-4">
+            @foreach ($movies as $movie)
+                <div class="col-4">
+                    <div class="card">
+                        <div>
+                            <strong>Title:</strong> {{ $movie->title }}
+                        </div>
+                        <div>
+                            <strong>Original Title:</strong> {{ $movie->original_title }} 
+                        </div>
+                        <div>
+                            <strong>Nationality:</strong> {{ $movie->nationality }}   
+                        </div>
+                        <div>
+                            <strong>Date:</strong> {{ $movie->date }} 
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
